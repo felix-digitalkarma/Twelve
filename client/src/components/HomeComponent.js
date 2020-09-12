431cd0ac5716cb78e520d9c10331d57f7a9bd4ad
felixmontanez <felix.montanez@gmail.com>
2020-09-11 21:15:02 -0700

    updated to include react-bootstrap and bootstrap libraries.

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 
+import hero from '../assets/getty_papermen.jpg';
 const HomeComponent = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <Fragment>
+      <Container fluid>
+        <Row>
+          <Col className="p-3"> <h1>Welcome to Twelve.Community</h1>
+            <span className="hero"><img src={hero} alt="Twelve Community" /></span>
+            <h5>Mission:</h5>
+            <p>To create a safe and secure platform for all to share personal stories of recovery, become involved with our communities through service, guiding others through sponsorship, and supporting our local and global communities through charitable donation.</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Ways you can help?</h3>
+            <ul>
+              <li><a href="https://forms.gle/FpfZnYYEWncwRDHy9" alt="Twelve Community">Take the survey</a></li>
+              <li><a href="https://docs.google.com/presentation/d/11yH55CqkhUpf8gpEN8-c5f7bDUa-Y6qzfAeY8AnPYpk/edit?usp=sharing">UX Process Presentation</a></li>
+              <li><a href="https://paypal.me/felixmontanez?locale.x=en_US" alt="Paypal Donate">Donate by Paypal</a></li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   )
 }
 
