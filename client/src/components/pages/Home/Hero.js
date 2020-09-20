ac54b0921f70bfda3204ca70bc9b9a6646b67ee6
felixmontanez <felix.montanez@gmail.com>
2020-09-19 21:24:50 -0700

    adding cleanup for ux process, clean up with class names

--- client/src/components/pages/Home/Hero.js
+++ client/src/components/pages/Home/Hero.js
@@ -6,40 +6,40 @@ const Hero = () => {
     <Container className="text-center">
       <Row>
         <Col>
-          <div id="myCarousel" class="carousel slide bg-none" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <div id="myCarousel" className="carousel slide bg-none" data-ride="carousel">
+            <ol className="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
               <li data-target="#myCarousel" data-slide-to="1"></li>
             </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#000" /></svg>
-                <div class="container">
-                  <div class="carousel-caption text-left">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#000" /></svg>
+                <div className="container">
+                  <div className="carousel-caption text-left">
                     <h1>Community Mission</h1>
                     <p>To create a safe and secure platform for all to share personal stories of recovery, become involved with our communities through service, guiding others through sponsorship, and supporting our local and global communities through charitable donation.</p>
-                    <p><a class="btn btn-lg btn-primary" href="https://forms.gle/VHt4Hrb9QGiBTVcWA" target="_blank" role="button">Take Survey</a></p>
+                    <p><a className="btn btn-lg btn-primary" href="https://forms.gle/VHt4Hrb9QGiBTVcWA" role="button">Take Survey</a></p>
                   </div>
                 </div>
               </div>
-              <div class="carousel-item">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#000" /></svg>
-                <div class="container">
-                  <div class="carousel-caption">
+              <div className="carousel-item">
+                <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#000" /></svg>
+                <div className="container">
+                  <div className="carousel-caption">
                     <h1>Find your recovery.</h1>
                     <p>Anonymity as a designing principle, recovery as a designing purpose, connection through modern technology.</p>
-                    <p><a class="btn btn-lg btn-primary" href="https://forms.gle/VHt4Hrb9QGiBTVcWA" target="_blank" role="button">Take Survey</a></p>
+                    <p><a className="btn btn-lg btn-primary" href="https://forms.gle/VHt4Hrb9QGiBTVcWA" target="_blank" role="button">Take Survey</a></p>
                   </div>
                 </div>
               </div>
             </div>
-            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
+            <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
+            <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
             </a>
           </div>
         </Col>
