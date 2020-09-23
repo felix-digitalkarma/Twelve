331b97a2f16ba70817dbb8b493d12e4248568a16
felixmontanez <felix.montanez@gmail.com>
2020-09-22 19:10:16 -0700

    cleanup for user stories

--- client/src/redux/modules/stories/components/StoryCard.js
+++ client/src/redux/modules/stories/components/StoryCard.js
@@ -11,13 +11,13 @@ const StoryCard = (story) => {
     <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
       <div className="col p-4 d-flex flex-column position-static">
         <strong className="d-inline-block mb-2 text-primary">{user.firstName} {user.lastInitial}</strong>
-        <h5 className="mb-0">{title}</h5>
-        <div className="mb-1 text-muted"><Moment format="lll">{shared}</Moment></div>
+        <h3 className="mb-0">{title}</h3>
+        <div className="mb-1 text-muted"><Moment format="ll">{shared}</Moment></div>
         <p className="card-text mb-auto">{body.substring(0, 100)}...</p>
         <Link to={`/stories/${_id}`} className="stretched-link">More</Link>
       </div>
       <div className="col-auto d-none d-lg-block">
-        <img className="bd-placeholder-img" width="200" height="250" src={url} />
+        <img className="bd-placeholder-img bg-dark" width="200" height="250" src={url} />
       </div>
     </div>
   )
