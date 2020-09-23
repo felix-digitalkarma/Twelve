b72564601f80e12c3567d84a1084cfd2b3a012d1
felixmontanez <felix.montanez@gmail.com>
2020-09-22 19:22:47 -0700

    cleanup for react-bootstrap refinement.

--- client/src/redux/modules/stories/components/StoryCard.js
+++ client/src/redux/modules/stories/components/StoryCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import Moment from 'react-moment';
 
 const StoryCard = (story) => {
@@ -8,18 +10,23 @@ const StoryCard = (story) => {
   const url = `https://picsum.photos/200/250?random=${random}`;
 
   return (
-    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-      <div className="col p-4 d-flex flex-column position-static">
-        <strong className="d-inline-block mb-2 text-primary">{user.firstName} {user.lastInitial}</strong>
-        <h5 className="mb-0">{title}</h5>
-        <div className="mb-1 text-muted"><Moment format="ll">{shared}</Moment></div>
-        <p className="card-text mb-auto">{body.substring(0, 100)}...</p>
-        <Link to={`/stories/${_id}`} className="stretched-link">More</Link>
-      </div>
-      <div className="col-auto d-none d-lg-block">
-        <img className="bd-placeholder-img bg-dark" width="200" height="250" src={url} />
-      </div>
-    </div>
+    <Container>
+      <Row className="no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <Col className="p-4 d-flex flex-column position-static">
+          <strong className="d-inline-block mb-2 text-primary">
+            {user.firstName} {user.lastInitial}
+          </strong>
+          <h5 className="mb-0">{title}</h5>
+          <div className="mb-1 text-muted"><Moment format="ll">{shared}</Moment></div>
+          <p className="card-text mb-auto">{body.substring(0, 100)}...</p>
+          <Link to={`/stories/${_id}`} className="stretched-link">More</Link>
+        </Col>
+        <div className="col-auto d-none d-lg-block">
+          <img className="bd-placeholder-img bg-dark" width="200" height="250" src={url} />
+        </div>
+      </Row>
+    </Container>
+
   )
 }
 
