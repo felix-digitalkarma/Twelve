44e04beea8efef4570ebe89be91678c06a63043f
felixmontanez <felix.montanez@gmail.com>
2020-09-30 18:48:21 -0700

    updated for production mode comparison

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // production mode
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile('index.html');
