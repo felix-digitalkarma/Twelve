ccdbef48fa92cad7317277242ab13ba383ed24d4
felixmontanez <felix.montanez@gmail.com>
2020-09-30 18:35:09 -0700

    reverting app usage of get

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/api/stories', require('./routes/Story.route'));
 // //production mode
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
-  app.post('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   })
 }
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 // //build mode
 if (process.env.NODE_ENV === 'development') {
   app.use(express.static(path.join(__dirname, 'public')));
-  app.post('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   })
 }
