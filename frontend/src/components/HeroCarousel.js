b16f4468d5f5a1520f8ff3be247b56858b1291e7
felix montanez <felix.montanez@gmail.com>
2020-10-05 14:51:53 -0700

    updated to have hero carousel component

---
+++ frontend/src/components/HeroCarousel.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import { Carousel } from "react-responsive-carousel";
+
+// import css
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+// assets
+import * as Img1 from "../assets/crp_unsplash.jpg";
+import * as Img2 from "../assets/daanstevens_group.jpg";
+
+const SlideContainer = styled.div`
+  img {
+    max-height: 420px;
+    object-fit: cover;
+  }
+`;
+
+const HeroCarousel = () => {
+  return (
+    <Carousel>
+      <SlideContainer>
+        <img src={Img1} width="400px" />
+        <p className="legend">meaningful personal recovery awareness</p>
+      </SlideContainer>
+      <SlideContainer>
+        <img src={Img2} />
+        <p className="legend">meaningful personal recovery awareness</p>
+      </SlideContainer>
+    </Carousel>
+  );
+};
+
+export default HeroCarousel;
