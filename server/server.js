381e9261c612df999af359f77b4e88276e3de31d
felix montanez <felix.montanez@gmail.com>
2020-10-11 02:30:21 -0700

    updated for cache control.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const cors = require('cors');
+const cors = require("cors");
 
-const express = require('express'),
-  path = require('path'),
-  bodyParser = require('body-parser'),
-  connectDB = require('../config/db');
-
+const express = require("express"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  connectDB = require("../config/db");
 
 const app = express();
 connectDB();
@@ -16,32 +15,62 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-
 // api
-const stories = require('./api/stories');
-const users = require('./api/users');
-const auth = require('./api/auth');
+const stories = require("./api/stories");
+const users = require("./api/users");
+const auth = require("./api/auth");
 
-app.use('/api/stories', stories);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+app.use("/api/stories", stories);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
-if (process.env.NODE_ENV === 'development') {
-  const staticDirectory = path.join(__dirname, '../frontend/public');
-  app.use(express.static(staticDirectory));
-  app.get('*', (req, res) => {
-    res.sendFile('index.html', { root: staticDirectory });
-  })
+if (process.env.NODE_ENV === "development") {
+  const staticDirectory = path.join(__dirname, "../frontend/public");
+  app.use(
+    express.static(staticDirectory, {
+      etag: true,
+      lastModified: true,
+      setHeaders: (res, path) => {
+        const hashRegExp = new RegExp("\\.[0-9a-f]{8}\\.");
+
+        if (path.endsWith(".html")) {
+          // All of the project's HTML files end in .html
+          res.setHeader("Cache-Control", "no-cache");
+        } else if (hashRegExp.test(path)) {
+          // If the RegExp matched, then we have a versioned URL.
+          res.setHeader("Cache-Control", "max-age=31536000");
+        }
+      },
+    })
+  );
+  app.get("*", (req, res) => {
+    res.sendFile("index.html", { root: staticDirectory });
+  });
 }
 
-
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // _dirname + '/app/frontend/build/index.html'
-  const staticDirectory = path.join(__dirname, '../frontend/build');
-  app.use(express.static(staticDirectory));
-  app.get('*', (req, res) => {
-    res.sendFile('index.html', { root: staticDirectory });
-  })
+  const staticDirectory = path.join(__dirname, "../frontend/build");
+  app.use(
+    express.static(staticDirectory, {
+      etag: true,
+      lastModified: true,
+      setHeaders: (res, path) => {
+        const hashRegExp = new RegExp("\\.[0-9a-f]{8}\\.");
+
+        if (path.endsWith(".html")) {
+          // All of the project's HTML files end in .html
+          res.setHeader("Cache-Control", "no-cache");
+        } else if (hashRegExp.test(path)) {
+          // If the RegExp matched, then we have a versioned URL.
+          res.setHeader("Cache-Control", "max-age=31536000");
+        }
+      },
+    })
+  );
+  app.get("*", (req, res) => {
+    res.sendFile("index.html", { root: staticDirectory });
+  });
 }
 
 const PORT = parseInt(process.env.PORT, 10) || 5000;
